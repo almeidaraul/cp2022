907ea43d06196b292fce0df3b503bde437d3ada4
Raul Almeida <almeidaraulgp@gmail.com>
2022-10-09 16:08:36 -0300

    add tracker stub

--- notebook/getlatex.sh
+++ notebook/getlatex.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cp header.tex pdf/notebook.tex
 cp notes.tex pdf/notes.tex
+cp tracker.tex pdf/tracker.tex
 python3 listings.py >> pdf/notebook.tex
 cd pdf
 pdflatex notebook.tex
 pdflatex notes.tex
+pdflatex tracker.tex
 mv *.pdf ../../
